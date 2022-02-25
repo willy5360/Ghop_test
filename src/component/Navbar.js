73bed499beaf3c846ae5be480960fa45b754b5fa
Williams Padilla Flores <willy5360@hotmail.com>
2022-02-25 21:39:35 +0100

    minor fixes

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,13 +6,19 @@ export const Navbar = () => {
         <nav className="Homebar bg-dark">
             <ul>
                 <li>
-                    <Link to="/home">Home</Link>
+                    <Link to="/home">
+                        <i className="fa-solid fa-house"></i>
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/perfil">Perfil</Link>
+                    <Link to="/perfil">
+                        <i className="fa-solid fa-user"></i>
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/ajustes">Ajustes</Link>
+                    <Link to="/ajustes">
+                        <i className="fa-solid fa-gear"></i>
+                    </Link>
                 </li>
             </ul>
         </nav>
